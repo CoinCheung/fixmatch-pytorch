18e91c62a07485151d78bcb9a615e70b9e9260cf
root <867153576@qq.com>
2020-02-03 07:58:31 +0000

    fix for switch

--- label_guessor.py
+++ label_guessor.py
@@ -7,10 +7,23 @@ class LabelGuessor(object):
         self.thresh = thresh
 
     @torch.no_grad()
-    def __call__(self, logits):
+    def __call__(self, model, ims):
+        org_state = {
+            k: v.clone().detach()
+            for k, v in model.state_dict().items()
+        }
+        is_train = model.training
+        model.train()
+        all_probs = []
+        logits = model(ims)
         probs = torch.softmax(logits, dim=1)
         scores, lbs = torch.max(probs, dim=1)
         lbs[scores < self.thresh] = self.discard_idx
 
+        model.load_state_dict(org_state)
+        if is_train:
+            model.train()
+        else:
+            model.eval()
         return lbs.detach()
 
