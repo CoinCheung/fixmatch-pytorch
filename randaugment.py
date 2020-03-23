dea5e04fa243fd5abd9ecda117ee2574ebe97e6e
root <867153576@qq.com>
2020-03-23 02:37:06 +0000

    fix contrast

--- randaugment.py
+++ randaugment.py
@@ -110,8 +110,9 @@ def contrast_func(img, factor):
     '''
         same output as PIL.ImageEnhance.Contrast
     '''
+    mean = np.sum(np.mean(img, axis=(0, 1)) * np.array([0.114, 0.587, 0.299]))
     table = np.array([(
-        el -74) * factor + 74
+        el - mean) * factor + mean
         for el in range(256)
     ]).clip(0, 255).astype(np.uint8)
     out = table[img]
